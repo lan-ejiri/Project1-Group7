82caf0b154c86e669bf30cef5f008d3c34042886
Mike Maroney <mikemaroney@customers-MacBook-Pro.local>
2018-03-30 14:55:13 -0400

    updated files to include login modal

--- app.js
+++ app.js
@@ -31,3 +31,14 @@ $(document).ready(function () {
 
 
 });
+
+
+// login modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+      modal.style.display = "none";
+  }
+}
