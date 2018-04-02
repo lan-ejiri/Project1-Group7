bd8fa7b658231784f3cbc09b037c80d37ad8fc6c
Lan Ejiri <lan.ejiri@yahoo.com>
2018-04-02 11:20:43 -0400

    log in functionality works now but doesnt show or hide the correct divs.... since its not connected to index yet

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDB7TGHzA9LfmcKOcrYnvZ6aLxBNf9JJnw",
-    authDomain: "project-1-group-7.firebaseapp.com",
-    databaseURL: "https://project-1-group-7.firebaseio.com",
-    projectId: "project-1-group-7",
-    storageBucket: "",
-    messagingSenderId: "138612540434"
-  };
-
-  firebase.initializeApp(config);
 
   var myIndex = 0;
   carousel();
