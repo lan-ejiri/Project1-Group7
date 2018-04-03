f5e3ae81af0374373ab4eb0c3d7839f3cfed4d14
Juan Valdivia <juan.valdivia18@gmail.com>
2018-04-03 10:55:59 -0400

    adding some backend for google login

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ function onSignIn(googleUser) {
   $("#post-login").css("display", "block");
   $("#pic").attr("src", profile.getImageUrl());
   $("#email").text(profile.getEmail());
+  $("#login-nav").css("display", "none");
+  $("#signup-nav").css("display", "none");
 }
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function() {
+    $("#login-nav").css("display", "block");
+    $("#signup-nav").css("display", "block");
     alert("You have been successfully signed out");
-
     $(".g-signin2").css("display", "block");
     $("#not-gmail-login").css("display", "block");
     $("#post-login").css("display", "none");
